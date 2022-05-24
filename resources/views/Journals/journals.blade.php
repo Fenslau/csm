f4faf2df94496acd6edb3a70a10084fa56ee1ae0
Fenslau <c01@bk.ru>
2022-05-24 15:46:27 +0700

    интерфейс главной страницы

--- resources/views/Journals/journals.blade.php
+++ resources/views/Journals/journals.blade.php
@@ -11,7 +11,7 @@
       {{ Breadcrumbs::render('journals') }}
       <div class="d-block h-50">
 
-            <nav class="d-sm-none navbar navbar-light">
+            <!-- <nav class="d-sm-none navbar navbar-light">
 
               <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"><i class="fas fa-bars fa-1x"></i> Журналы</button>
 
@@ -32,24 +32,16 @@
                   </li>
                 </ul>
               </div>
-            </nav>
+            </nav> -->
 
-            <nav class="d-none d-sm-flex justify-content-around align-items-stretch navbar">
+            <nav class="d-flex justify-content-around align-items-stretch navbar">
               <div class="m-1 min-content alert alert-success">
-                <p class="d-none d-md-block text-center lh-sm mb-0"><small>Регистрация и контроль температурного режима</small></p>
+                <p class="text-center lh-sm mb-0"><small>Регистрация и контроль температурного режима</small></p>
                 <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('journal-holod') }}">холодильника</a>
               </div>
               <div class="m-1 min-content alert alert-success">
-                <p class="d-none d-md-block text-center lh-sm mb-0"><small>учёт работы</small></p>
+                <p class="text-center lh-sm mb-0"><small>учёт работы</small></p>
                 <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('journal-lampa') }}">бактерицидной установки</a>
-              </div>
-              <div class="m-1 min-content alert alert-success">
-                <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="#">...</a>
-                <p class="d-none d-md-block text-center lh-sm"><small></small></p>
-              </div>
-              <div class="m-1 min-content alert alert-success">
-                <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="#">...</a>
-                <p class="d-none d-md-block text-center lh-sm"><small></small></p>
               </div>
             </nav>
       </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,9 @@
     <div class="col">
       {{ Breadcrumbs::render('home') }}
       <div style="height: 75vh;" class="position-relative">
-          <div class="d-block h-50">
+          <div class="d-block">
 
-                <nav class="d-sm-none navbar navbar-light">
+                <!-- <nav class="d-sm-none navbar navbar-light">
 
                   <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"><i class="fas fa-bars fa-1x"></i> Меню</button>
 
@@ -33,24 +33,24 @@
                       </li>
                     </ul>
                   </div>
-                </nav>
+                </nav> -->
 
-                <nav class="d-none d-sm-flex justify-content-around align-items-stretch navbar">
-                  <div class="m-1 min-content alert alert-success">
+                <nav class="d-flex justify-content-around align-items-stretch navbar">
+                  <!-- <div class="m-1 min-content alert alert-success">
                     <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('document') }}">Электронный документооборот</a>
                     <p class="d-none d-md-block text-center lh-sm"><small>корреспонденция,	договоры, внутрикорпоративные документы</small></p>
                   </div>
                   <div class="m-1 min-content alert alert-success">
                     <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('procedure') }}">Процедурный кабинет</a>
                     <p class="d-none d-md-block text-center lh-sm"><small>регистрация манипуляций</small></p>
-                  </div>
+                  </div> -->
                   <div class="m-1 min-content alert alert-success">
                     <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('journals') }}">Журналы</a>
-                    <p class="d-none d-md-block text-center lh-sm"><small>холодильники, уборка помещений, кварцевание и т.п</small></p>
+                    <p class="text-center lh-sm"><small>холодильники, уборка помещений, кварцевание и т.п</small></p>
                   </div>
                   <div class="m-1 min-content alert alert-success">
                     <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('otkaz') }}">Регистрация отказов</a>
-                    <p class="d-none d-md-block text-center lh-sm"><small>отказы клиники в запросах пациентов на медицинские услуги</small></p>
+                    <p class="text-center lh-sm"><small>отказы клиники в запросах пациентов на медицинские услуги</small></p>
                   </div>
                 </nav>
           </div>
@@ -58,7 +58,7 @@
 
 
           <div class="">
-            <h5 class="d-none d-md-block text-center pt-3">Администрирование</h5>
+            <!-- <h5 class="d-none d-md-block text-center pt-3">Администрирование</h5>
             <nav class="d-sm-none navbar navbar-light">
 
               <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2"><i class="fas fa-bars fa-1x"></i> Административное Меню</button>
@@ -71,13 +71,13 @@
                 </ul>
               </div>
 
-            </nav>
+            </nav> -->
 
-            <nav class="d-none d-sm-flex justify-content-around align-items-stretch navbar">
+            <nav class="d-flex justify-content-around align-items-stretch navbar">
 
               <div class="m-1 min-content alert alert-danger">
                 <a class="nav-link stretched-link alert-link rounded font-weight-bolder lh-m text-center text-uppercase" href="{{ route('users') }}">Управление пользователями</a>
-                <p class="d-none d-md-block text-center lh-sm"><small>контроль пользователей, выдача ролей, прав</small></p>
+                <p class="text-center lh-sm"><small>контроль пользователей, выдача ролей, прав</small></p>
               </div>
 
             </nav>
